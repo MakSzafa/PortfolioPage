0c28dbaa395329bf13b3e157e434ae3eabc76077
Max Szafarski <maksymilian.szafarski@vocale.app>
2022-05-02 11:31:19 +0200

    fix email redirection

--- mail_handler.php
+++ mail_handler.php
@@ -1,7 +1,5 @@
 <?php 
 if(isset($_POST['submit'])){
-    header('Location:error404.html');
-
     $to = "kontakt@makszafa.pl";
     $from = $_POST['mail'];
     $name = $_POST['name'];
@@ -13,12 +11,8 @@ if(isset($_POST['submit'])){
 
     if( $retval == true ) {
         header('Location:email_sent.html');
-        sleep(5);
-        header('Location: ' . $_SERVER['HTTP_REFERER']);
      }else {
         header('Location:email_error.html');
-        sleep(5);
-        header('Location: ' . $_SERVER['HTTP_REFERER']);
      }
     }
 ?>
